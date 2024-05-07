cea3fbc9fb88279a24f2def1a512fc02b952703e
ZiBenty <bentivoglio.matteo@libero.it>
2024-05-07 10:46:56 +0200

    rune, type and cost card template load
    succesfull

--- Assets/_app/Scripts/CardTemplateFiller.cs
+++ Assets/_app/Scripts/CardTemplateFiller.cs
@@ -13,37 +13,103 @@ public class CardTemplateFiller : MonoBehaviour
     public TMP_Text Stars;
     public TMP_Text Attack;
     public TMP_Text Life;
+    public List<Image> Cost;
+    /*
+    0=top-left
+    1=top
+    2=top-right
+    3=bottom-left
+    4=bottom
+    5=bottom-right
+    */
+    public GameObject CostHelper;
+    
+
+    public List<Sprite> RuneSprites;
+    /*
+    0=None
+    1=Fire
+    2=Earth
+    3=Air
+    4=Water
+    5=Ancestral
+    */
+    public List<Sprite> TypeSprites;
+    /*
+    0=None
+    1=Creature
+    2=Structure
+    3=Enchantment
+    */
+    void Start(){
+        Rune.sprite = RuneSprites[0];
+        Type.sprite = TypeSprites[0];
+    }
 
     public void changeName(TMP_InputField Input){
         Name.text = Input.text.ToString();
     }
 
     public void changeRune(TMP_Text Input){
-        Rune.sprite = Resources.Load<Sprite>("Assets/_app/Sprites/" + Input.text.ToString());
+        switch(Input.text.ToString()){
+            case "Fire":
+                Rune.sprite = RuneSprites[1];
+                break;
+            case "Earth":
+                Rune.sprite = RuneSprites[2];
+                break;
+            case "Air":
+                Rune.sprite = RuneSprites[3];
+                break;
+            case "Water":
+                Rune.sprite = RuneSprites[4];
+                break;
+            case "Ancestral":
+                Rune.sprite = RuneSprites[5];
+                break;
+            default:
+                Rune.sprite = RuneSprites[0];
+                break;
+        }
     }
 
     public void changeType(TMP_Text Input){
-        Type.sprite = Resources.Load<Sprite>("Assets/_app/Sprites/" + Input.text.ToString());
+        switch(Input.text.ToString()){
+            case "Creature":
+                Type.sprite = TypeSprites[1];
+                break;
+            case "Structure":
+                Type.sprite = TypeSprites[2];
+                break;
+            case "Enchantment":
+                Type.sprite = TypeSprites[3];
+                break;
+            default:
+                Type.sprite = TypeSprites[0];
+                break;
+        }
     }
 
     public void changeStars(TMP_InputField Input){
-        int num = int.Parse(Input.text.ToString());
-        if(num > 0 && num <=3){
-            switch(num){
-                case 1:
-                    Stars.text = "*";
-                    break;
-                case 2:
-                    Stars.text = "**";
-                    break;
-                case 3:
-                    Stars.text = "***";
-                    break;
+        if(Input.text.ToString()!=""){
+            int num = int.Parse(Input.text.ToString());
+            if(num > 0 && num <=3){
+                switch(num){
+                    case 1:
+                        Stars.text = "*";
+                        break;
+                    case 2:
+                        Stars.text = "**";
+                        break;
+                    case 3:
+                        Stars.text = "***";
+                        break;
+                }
+            }else if (num>3){
+                Stars.text = "***";
+            }else{
+                Stars.text = "";
             }
-        }else if (num>3){
-            Stars.text = "***";
-        }else{
-            Stars.text = "";
         }
     }
 
@@ -57,6 +123,16 @@ public class CardTemplateFiller : MonoBehaviour
 
     public void changeLife(TMP_InputField Input){
         Life.text = Input.text.ToString();
+    }
+
+    public void changeCost(){
+        List<int> CCI = CostHelper.GetComponent<CostHelper>().CostCodeIndex;
+        Cost[0].sprite = RuneSprites[CCI[0]];
+        Cost[1].sprite = RuneSprites[CCI[1]];
+        Cost[2].sprite = RuneSprites[CCI[2]];
+        Cost[3].sprite = RuneSprites[CCI[3]];
+        Cost[4].sprite = RuneSprites[CCI[4]];
+        Cost[5].sprite = RuneSprites[CCI[5]];
     }
 
 }
